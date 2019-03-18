c91a04cd0ddff6af33d8be9a9e23b191c3ca069b
David R. Bild <david.bild@xaptum.com>
2019-03-17 20:45:02 -0500

    captived: add general aggregate resource

---
+++ captived/src/rest/aggregate_resource.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <functional>
+#include <string>
+#include <vector>
+
+#include "rest/resource.hpp"
+
+namespace captived {
+namespace rest {
+
+class aggregate_resource : public resource {
+  public:
+  aggregate_resource(std::string path);
+
+  void add(std::string name, resource& res);
+
+  resp_type get(req_type) override;
+
+  private:
+  std::vector<
+    std::tuple<std::string,
+               std::reference_wrapper<resource>
+               >
+    > children_;
+
+};
+
+} // namespace rest
+} // namespace captived
