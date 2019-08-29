7e2d09fbb66354346e2877e0e97f7bc3d3f6cbf1
Gergely Fejer <gfejer@gmail.com>
2019-08-29 16:52:21 -0500

    Changed command name.

--- captived/src/defines.hpp
+++ captived/src/defines.hpp
@@ -30,7 +30,7 @@ const std::string PATH_MODE_TARGET = "/etc/systemd/system";
 
 const std::string COMMAND_GET_ACTIVE_TARGETS =
     "/bin/systemctl list-units --type target";
-const std::string COMMAND_CONNMAN_SIGHUP =
+const std::string COMMAND_RELOAD_CONNMAN =
     "/bin/systemctl kill -s HUP connman.service";
 const std::string COMMAND_GET_SSID = "/sbin/iw dev wlan0 link";
 
