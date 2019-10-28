c2d451f2eef1729120107ddc3ea16c6629a6171d
Gergely Fejer <gfejer@gmail.com>
2019-10-28 16:24:39 -0500

    Changed regex to be more general.

--- captived/src/rest/model.cpp
+++ captived/src/rest/model.cpp
@@ -13,7 +13,7 @@ model::model_number() {
     if (!line_opt)
         return {};
 
-    std::regex re("artifact_name=(\\w+(-dev)?)");
+    std::regex re("artifact_name=([\\w-]+)-v\\d+\\.\\d+\\.\\d+-\\d+-g[0-9a-hA-H]+$");
     std::smatch match;
     std::regex_search(*line_opt, match, re);
     if (match.size() < 1)
